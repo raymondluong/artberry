3c628241b8d174ca11de25ada02c98e73549d55e
Na He Jeon <nahejeon@rescomp-16-303253.stanford.edu>
2016-12-05 21:20:30 -0800

    added all the artberry colors (tintColor, redBerry, blueBerry, yellowBerry, orangeBerry)

--- app/constants/Colors.js
+++ app/constants/Colors.js
@@ -1,7 +1,15 @@
 const tintColor = '#752775';
+const redBerry = '#D993CE';
+const blueBerry = 'A289E7';
+const yellowBerry = 'FFD68C';
+const orangeBerry = 'FFA846';
 
 export default {
   tintColor,
+  redBerry,
+  blueBerry,
+  yellowBerry,
+  orangeBerry,
   tabIconDefault: '#888',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
