fc4aa0bd6d45c73863758dd8ccc5c5bdb3c94cfa
Raymond Luong <rayluong92@gmail.com>
2016-12-06 17:16:40 -0800

    cleaned up redundant code and fixed centering issue

--- app/components/PlaylistItem.js
+++ app/components/PlaylistItem.js
@@ -12,40 +12,16 @@ export class PlaylistItem extends React.Component {
     let details = this.props.playlist;
 
     return (
-      <View style={[styles.container, this.props.style]}>
-        <View style={styles.imageContainer}>
-          <Image source={details.image} style={styles.image} />
-        </View>
+      <View style={this.props.style}>
+        <Image source={details.image} style={styles.image} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 10,
-    marginBottom: 0,
-  },
-  imageContainer: {
-    margin: 0,
-  },
   image: {
     width: 160,
     height: 160,
-    paddingTop: 0
-  },
-  titleContainer: {
-    marginBottom: 0,
-  },
-  titleText: {
-    fontWeight: 'bold',
-    fontSize: 20
-  },
-  dateText: {
-    fontSize: 15
   }
 });
