d6e21f6e259a3ffa3160f82d31758c88a66e814d
Raymond Luong <rayluong92@gmail.com>
2016-12-08 16:01:09 -0800

    modified helper to include hour display

--- app/constants/Helpers.js
+++ app/constants/Helpers.js
@@ -1,20 +1,23 @@
 function secToDisplay(sec) {
-  var minutes = Math.floor(sec / 60);
+  var hours = Math.floor(sec / 3600);
+  var minutes = Math.floor((sec % 3600) / 60);
   var seconds = sec % 60;
-  var minutesString = `${minutes} minutes`;
-  if (minutes < 1) {
-    minutesString = '';
-  }
-  var secondsString = `${seconds} seconds`;
-  if (seconds === 0) {
-    secondsString = '';
-  } else if (seconds === 1) {
-    secondsString = `${seconds} second`;
-  }
-  if (minutesString && secondsString) {
+
+  var hoursString = hours > 1 ? `${hours} hours` : hours === 1 ? `${hours} hour` : '';
+  var minutesString = minutes > 1 ? `${minutes} minutes` : minutes === 1 ? `${minutes} minute` : '';
+  var secondsString = seconds > 1 ? `${seconds} seconds` : seconds === 1 ? `${seconds} second` : '';
+
+  if (hoursString && minutesString && secondsString) {
+    return `${hoursString}, ${minutesString}, and ${secondsString}`;
+  } else if (minutesString && secondsString) {
     return `${minutesString} and ${secondsString}`;
+  } else if (hoursString && secondsString) {
+    return `${hoursString} and ${secondsString}`;
+  } else if (hoursString && minutesString) {
+    return `${hoursString} and ${minutesString}`;
   }
-  return minutesString + secondsString;
+  
+  return hoursString + minutesString + secondsString;
 }
 
 export default {
